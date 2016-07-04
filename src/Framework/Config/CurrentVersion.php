c03b77823da8385ff651fbd0854aee05eeaa87d2
Xabier Legasa <xabierlegasa@gmail.com>
2016-07-04 21:47:26 +0200

    Refactor version output

--- src/Framework/Config/CurrentVersion.php
+++ src/Framework/Config/CurrentVersion.php
@@ -27,11 +27,21 @@ class CurrentVersion
     {
         try {
             $version = file_get_contents(__DIR__ . '/../../../' . $this->config->get('current_version_file'));
+
+            if (strlen($version) === 15) {
+                $version = substr($version, 0, 4)
+                    . '-' . substr($version, 4, 2)
+                    . '-' . substr($version, 6, 2)
+                    . ' ' . substr($version, 8, 2)
+                    . ':' . substr($version, 10, 2)
+                    . ' - ' . substr($version, 12, 3)
+                ;
+            }
         } catch (\Exception $e) {
             $this->logger->error('Error trying to obtain current version. Error: ' . $e->getMessage());
             $version = 'unknown_version';
         }
-        
+
         return $version;
     }
 }
