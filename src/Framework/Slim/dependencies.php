f87c87f5a377152551a7fed419bd98fb0df814fa
Xabier Legasa <xabierlegasa@gmail.com>
2016-07-04 16:31:27 +0200

    Remove twig. Clean up old files

--- src/Framework/Slim/dependencies.php
+++ src/Framework/Slim/dependencies.php
@@ -30,26 +30,6 @@ $container['renderer'] = function ($c) {
     return new Slim\Views\PhpRenderer($settings['template_path']);
 };
 
-
-//// Register slim/php-view component on container
-//$container['view'] = function ($c) {
-//    return new \Slim\Views\PhpRenderer('../templates/');
-//};
-
-//// twig
-//$container['view'] = function ($container) {
-//    $view = new \Slim\Views\Twig('../templates', [
-//        //'cache' => '../templates/cache'
-//        'cache' => false
-//    ]);
-//    $view->addExtension(new \Slim\Views\TwigExtension(
-//        $container['router'],
-//        $container['request']->getUri()
-//    ));
-//
-//    return $view;
-//};
-
 $container['UsersRepository'] = function ($c) {
     /** @var Config $config */
     $config = $c->get('settings')['config'];
